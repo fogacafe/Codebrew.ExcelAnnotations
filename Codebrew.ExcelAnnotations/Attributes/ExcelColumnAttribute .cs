627f9ac3b0f8fdf352c670e317dc87069371ab64
fogacafe <felipe.foogaca@gmail.com>
2025-05-02 22:43:50 -0300

    feat: add exporter

---
+++ Codebrew.ExcelAnnotations/Attributes/ExcelColumnAttribute .cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codebrew.ExcelAnnotations.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExcelColumnAttribute : Attribute
+    {
+        public string ColumnName { get; }
+        public ExcelColumnAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+    }
+}
---
+++ Codebrew.ExcelAnnotations/Attributes/ExcelSheetAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Codebrew.ExcelAnnotations.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ExcelSheetAttribute : Attribute
+    {
+        public string SheetName { get; }
+
+        public ExcelSheetAttribute(string sheetName)
+        {
+            SheetName = sheetName;
+        }
+    }
+}
