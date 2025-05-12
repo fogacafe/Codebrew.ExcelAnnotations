246e1239bdfdc172e31230174eda3a4e7a0b7ef1
fogacafe <felipe.foogaca@gmail.com>
2025-05-11 22:05:07 -0300

    feat: importer example

--- Codebrew.ExcelAnnotations.Example/Program.cs
+++ Codebrew.ExcelAnnotations.Example/Program.cs
@@ -6,6 +6,38 @@ using Codebrew.ExcelAnnotations.Example.Worksheets;
 
 try
 {
+    Console.WriteLine("Creating");
+    Create();
+    Console.WriteLine("Created");
+
+    Console.WriteLine("Importing");
+    var items = Import<BookWorksheet>();
+    Console.WriteLine("Imported");
+
+    Console.WriteLine("Exporting");
+    Export(items);
+    Console.WriteLine("Exported");
+
+
+    Console.WriteLine("Finished");
+    Console.ReadLine();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
+
+static List<T> Import<T>() where T : new()
+{
+    using(var importer = new Importer("excel.xlsx"))
+    {
+        return importer.Import<T>(new WorksheetOptions()).ToList();
+    }
+}
+
+static void Create()
+{
     var books = new List<BookWorksheet>()
     {
         new() { Title = "Apple", Date = new DateTime(2000, 01, 01), Percentage = 50m, Price = 200, IsActive = null},
@@ -13,17 +45,15 @@ try
         new() { Title = "Hexagonal", Date = new DateTime(2015, 01, 01), Percentage = 95m, Price = 50, IsActive = false}
     };
 
-
-    var exporter = new Exporter();
-
-    exporter.MapToWorksheet(books, new WorksheetOptions());
-    exporter.MapToWorksheet(books, new WorksheetOptions() { WorksheetName = "Books25" });
-    exporter.ExportToPath("excel.xlsx");
-    Console.WriteLine("Finished");
+    Export(books);
 }
-catch(Exception ex)
+
+static void Export<T>(IEnumerable<T> items)
 {
-    Console.WriteLine(ex.ToString());
+    using (var exporter = new Exporter())
+    {
+        exporter.MapToWorksheet(items, new WorksheetOptions());
+        exporter.MapToWorksheet(items, new WorksheetOptions() { WorksheetName = "Books25" });
+        exporter.ExportToPath("excel.xlsx");
+    }
 }
-
-Console.ReadLine();
